7a415290131ba01a46ce70f11bf4b059c862a130
Ola Flisbäck <ola.flisback@gmail.com>
2024-11-19 21:32:01 +0100

    Towards checkout/reset to branches via telescope

--- lua/cursor-git-ref-command/commands.lua
+++ lua/cursor-git-ref-command/commands.lua
@@ -1,6 +1,42 @@
 local M = {}
+local pickers = require("telescope.pickers")
+local finders = require("telescope.finders")
+local conf = require("telescope.config").values
+local actions = require("telescope.actions")
+local action_state = require("telescope.actions.state")
 
 M.register = function()
+	local function extract_refs(line)
+		local refs = {}
+
+		local start_paren = line:find("%(")
+		if not start_paren then
+			return refs
+		end
+
+		local end_paren = line:find("%)", start_paren)
+		if not end_paren then
+			return refs
+		end
+
+		local refs_str = line:sub(start_paren + 1, end_paren - 1)
+
+		for ref in refs_str:gmatch("[^,]+") do
+			ref = ref:match("^%s*(.-)%s*$")
+
+			-- If the ref contains " -> ", get the target ref
+			local target_ref = ref:match("->%s*(.+)")
+			if target_ref then
+				ref = target_ref
+			end
+
+			if ref ~= "HEAD" and ref ~= "origin/HEAD" then
+				table.insert(refs, ref)
+			end
+		end
+		return refs
+	end
+
 	local function extract_word(line, start_pos)
 		local start = start_pos
 		while start > 0 and line:sub(start, start):match("%w") do
@@ -36,14 +72,16 @@ M.register = function()
 
 		if not is_valid_commit then
 			print("Invalid commit hash or not within a valid git repository: " .. commit_hash)
-			return
+			return nil, {}
 		end
 
-		return commit_hash
+		local refs = extract_refs(line)
+
+		return commit_hash, refs
 	end
 
 	function CherryPick()
-		local commit_hash = getCursorHash()
+		local commit_hash, _ = getCursorHash()
 
 		local cherry_pick_cmd = string.format("git cherry-pick %s", commit_hash)
 		local result = vim.fn.system(cherry_pick_cmd)
@@ -56,7 +94,7 @@ M.register = function()
 	end
 
 	function Drop()
-		local commit_hash = getCursorHash()
+		local commit_hash, _ = getCursorHash()
 
 		local parent_commit_cmd = string.format("git rev-list --parents -n 1 %s", commit_hash)
 		local rev_list_result = vim.fn.system(parent_commit_cmd)
@@ -90,7 +128,7 @@ M.register = function()
 	}
 
 	function Reset(mode)
-		local commit_hash = getCursorHash()
+		local commit_hash, _ = getCursorHash()
 
 		if not commit_hash or commit_hash == "" then
 			print("No valid commit hash provided.")
@@ -107,21 +145,67 @@ M.register = function()
 		end
 	end
 
+	-- This is so close to working. Isn't the commit selected properly? I think it is. Why isn't it checked out
+	-- and when done, deduplicate the checkout logic.
+	-- and when done with that, also do similar thing for reset functions they should also be able to reset to branches
 	function CheckOut()
-		local commit_hash = getCursorHash()
+		local commit_hash, refs = getCursorHash()
 
 		if not commit_hash or commit_hash == "" then
 			print("No valid commit hash provided.")
 			return
 		end
 
-		local reset_cmd = string.format("git checkout %s", commit_hash)
-		local result = vim.fn.system(reset_cmd)
+		local selected_ref = nil
 
-		if vim.v.shell_error ~= 0 then
-			print("Failed to checkout commit:", result)
+		if #refs > 0 then
+			-- Create a list of options including the commit hash and all refs
+			local options = { commit_hash }
+			for _, ref in ipairs(refs) do
+				table.insert(options, ref)
+			end
+
+			-- Show telescope picker
+			local picker = pickers.new({}, {
+				prompt_title = "Select ref to checkout",
+				finder = finders.new_table({
+					results = options,
+				}),
+				sorter = conf.generic_sorter({}),
+				attach_mappings = function(prompt_bufnr)
+					actions.select_default:replace(function()
+						local selection = action_state.get_selected_entry()
+						selected_ref = selection[1]
+						print("assigned selected_Ref to: " .. selected_ref)
+						actions.close(prompt_bufnr)
+					end)
+					return true
+				end,
+			})
+			picker:find()
+			if selected_ref == nil then
+				print("selected_ref is nil")
+				return
+			end
+			print("selected_ref: " .. selected_ref)
+			local reset_cmd = string.format("git checkout %s", selected_ref)
+			local result = vim.fn.system(reset_cmd)
+
+			if vim.v.shell_error ~= 0 then
+				print("Failed to checkout commit:", result)
+			else
+				print("Checked out:", commit_hash)
+			end
 		else
-			print("Checked out:", commit_hash)
+			-- deduplicate this
+			local reset_cmd = string.format("git checkout %s", commit_hash)
+			local result = vim.fn.system(reset_cmd)
+
+			if vim.v.shell_error ~= 0 then
+				print("Failed to checkout commit:", result)
+			else
+				print("Checked out:", commit_hash)
+			end
 		end
 	end
 end
